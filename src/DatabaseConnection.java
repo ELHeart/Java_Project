0a229c8fd4b1c2ffc5d1e0c182eccfca28ac208b
Davidson <tobysonevangel@gmail.com>
2025-03-04 22:02:30 +0000

    MySQL JDBC driver exception edit

--- src/DatabaseConnection.java
+++ src/DatabaseConnection.java
@@ -1,12 +1,20 @@
 import java.sql.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class DatabaseConnection {
+    private static final Logger logger = LoggerFactory.getLogger(DatabaseConnection.class);
     private static final String DB_URL = "jdbc:mysql://localhost:3306/club_membership";
     private static final String DB_USER = "root";
     private static final String DB_PASSWORD = "";
 
-    public static Connection getConnection() throws SQLException, ClassNotFoundException {
-        Class.forName("com.mysql.cj.jdbc.Driver");
+    public static Connection getConnection() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            logger.error("MySQL JDBC Driver not found. Make sure you have mysql-connector-j added to your project", e);
+            throw new SQLException("MySQL JDBC Driver not found. Please add mysql-connector-j to your project's libraries.", e);
+        }
         return DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
     }
 }
