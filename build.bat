5811f4e473f5a7844f9c058cb9c31b5657b690f3
Davidson <tobysonevangel@gmail.com>
2025-03-10 05:32:10 +0000

    gg

--- build.bat
+++ build.bat
@@ -8,14 +8,28 @@ if not exist dist\lib mkdir dist\lib
 
 :: Copy all required JAR files to lib directory
 echo Copying JAR files...
-copy "lib\mysql-connector-j-9.2.0\mysql-connector-j-9.2.0.jar" "dist\lib\"
-copy "lib\slf4j-api-2.0.9\slf4j-api-2.0.9.jar" "dist\lib\"
-copy "lib\logback-core-1.5.17\logback-core-1.5.17.jar" "dist\lib\"
-copy "lib\logback-classic-1.5.17\logback-classic-1.5.17.jar" "dist\lib\"
+
+:: MySQL Connector
+if exist "lib\mysql-connector-j-9.2.0\mysql-connector-j-9.2.0.jar" (
+    copy "lib\mysql-connector-j-9.2.0\mysql-connector-j-9.2.0.jar" "dist\lib\" > nul
+    echo MySQL Connector copied successfully
+) else (
+    echo ERROR: MySQL Connector JAR not found
+    pause
+    exit /b 1
+)
+
+:: Set the classpath
+set CLASSPATH=src;dist\lib\mysql-connector-j-9.2.0.jar
 
 echo Compiling Java files...
-:: Compile Java files with explicit classpath
-javac -d bin -cp "dist\lib\mysql-connector-j-9.2.0.jar;dist\lib\slf4j-api-2.0.9.jar;dist\lib\logback-core-1.5.17.jar;dist\lib\logback-classic-1.5.17.jar;src" src\*.java
+:: Compile Java files
+javac -d bin -cp "%CLASSPATH%" src\*.java
+if errorlevel 1 (
+    echo Compilation failed
+    pause
+    exit /b 1
+)
 
 :: Create JAR file
 echo Creating JAR file...
@@ -26,9 +40,18 @@ cd ..
 :: Copy other required files
 echo Copying other required files...
 copy ClubMembershipApp.jar dist\
-copy src\logback.xml dist\
+if exist src\logback.xml copy src\logback.xml dist\
 if not exist dist\images mkdir dist\images
-copy images\*.* dist\images\
+if exist images\*.* copy images\*.* dist\images\
+
+:: Create run.bat in dist folder
+echo Creating run.bat...
+echo @echo off > dist\run.bat
+echo java -jar ClubMembershipApp.jar >> dist\run.bat
+echo pause >> dist\run.bat
 
 echo Done! The executable can be found in the dist folder.
+echo To run the application:
+echo 1. Go to the dist folder
+echo 2. Double-click run.bat
 pause 
